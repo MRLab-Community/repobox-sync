bc80c219bf812dc3380ad73a295f9ee2daeb5d17
MRLab-Community <mrmp@hotmail.it>
2026-03-20 10:00:02 +0100

    Sync from Repobox (Selective) - 2026-03-20 10:00:01

---
+++ files+da+analizzare/repobox/config_db.php
@@ -0,0 +1,6 @@
+<?php
+// Configurazione Database Generata da Installer
+define('REPO_DB_HOST', 'localhost');
+define('REPO_DB_NAME', 'my_myprogect');
+define('REPO_DB_USER', 'myprogect');
+define('REPO_DB_PASS', 'Gold..79463572');
